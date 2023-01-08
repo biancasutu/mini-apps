1e10a22f603254dca0128190e56fa5768b624a0d
biancasutu <bianca.andreea.sutu@gmail.com>
2023-01-08 23:57:47 +0200

    i created the first Snake game using Turtle module

--- snake_games/snake1.py
+++ snake_games/snake1.py
@@ -4,6 +4,11 @@ import random
 
 delay = 0.1
 
+# Count Score
+score = 0
+high_score = 0
+
+
 # Set up the screen
 window = turtle.Screen()
 window.title('Snake')
@@ -35,18 +40,33 @@ food.goto(0,0)  # the game starts from the center of the screen
 
 segments = [] # we create a list
 
+# Pen - for scoring and writing
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape('square')
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write('Score: 0  High Score: 0', align='center', font=('Courier', 24, 'normal'))
+
+
 # Functions to make our snake move on the xOy axis
 def move_up():
-    head.direction = 'up'
+    if head.direction != 'down':
+        head.direction = 'up'
 
 def move_down():
-    head.direction = 'down'
+    if head.direction != 'up':
+        head.direction = 'down'
 
 def move_right():
-    head.direction = 'right'
+    if head.direction != 'left':
+        head.direction = 'right'
 
 def move_left():
-    head.direction = 'left'
+    if head.direction != 'right':
+        head.direction = 'left'
 
 def move():
     if head.direction == 'up':
@@ -78,6 +98,27 @@ window.onkeypress(move_left, "a")
 while True:
     window.update()
 
+    # check for collision with the borders of screen
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = 'stop'
+
+        # hide existing segments (body of snake) after colliding with the border
+        for segment in segments:
+            segment.goto(1000, 1000)
+
+        # clear the segments (start again with no body)
+        segments.clear()
+
+        # Reset the score
+        score = 0
+        pen.clear()
+        pen.write(f'Score: {score}  High Score: {high_score}', align="center", font=("Courier", 24, "normal"))
+        
+        # reset the delay
+        delay = 0.1
+
     # check if the snake touches the food
     if head.distance(food) < 20:
         # move the food to a random position on screen
@@ -87,13 +128,28 @@ while True:
         y = random.randrange(-240, 240, 20)
         food.goto(x, y)
 
-        # add a segment in the body
+        # add a segment in the body after the snake eats
         new_segment = turtle.Turtle()
         new_segment.speed(0)
         new_segment.shape('square')
         new_segment.color('green')
         new_segment.penup()
         segments.append(new_segment)
+
+        # shorten the delay as the snake eats (because Turtle slows down when the
+        #  nr of items on the screen is increased )
+        delay -= 0.001
+
+        # Increase the score
+        score += 10 # adds 10 points to score after eating 
+
+        if score > high_score:
+            high_score = score
+        
+        pen.clear()
+        pen.write(f'Score: {score}  High Score: {high_score}', align="center", font=("Courier", 24, "normal"))
+
+    
 
     #Move the end segments first in reverse order
     # Adding each segment after the snake ate behind the head (beggining of the body/tale)
@@ -109,9 +165,28 @@ while True:
         y = head.ycor()
         segments[0].goto(x, y)
 
-
-
     move()
+
+    # Check if snake eats itself (head touches the body)
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = 'stop'
+        
+            # hide existing segments (body of snake) after colliding with the border
+            for segment in segments:
+                segment.goto(1000, 1000)
+
+            # clear the segments (start again with no body)
+            segments.clear()
+
+            score = 0
+            pen.clear()
+            pen.write(f'Score: {score}  High Score: {high_score}', align="center", font=("Courier", 24, "normal"))
+            
+            # reset the delay
+            delay = 0.1
 
     time.sleep(delay)
 
@@ -122,3 +197,4 @@ window.mainloop()  # keeps the window open
 # Notes :
 # the food's coordinates must be multiple of 20px
 # the snake cannot move directly in the opposite direction because he would eat his tale
+# the food should appear randomly on the empty screen (not where the snake is)
